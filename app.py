537eaef18f28463c0a96bfa0f89195059201e476
Rohandas17 <rohandas45017@gmail.com>
2025-10-03 03:20:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import io
+
+df = pd.read_csv('/kaggle/input/aqi-air-quality-index-scheduled-daily-update/data_date.csv', on_bad_lines='skip')
+df['Date'] = pd.to_datetime(df['Date'])
+df['Year'] = df['Date'].dt.year
+df['Month'] = df['Date'].dt.month
+df['Day'] = df['Date'].dt.day
+
+df = df.drop('Date', axis=1)
+df = pd.get_dummies(df, columns=['Country', 'Status'], drop_first=True)
+
+X = df.drop('AQI Value', axis=1)
+y = df['AQI Value']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f"Mean Squared Error: {mse}")
+print(f"R-squared: {r2}")
